c3f4bcce3a2624355da4197d68edb4f6d30392a1
wisdomamopho <67025792+wisdomamopho@users.noreply.github.com>
2024-12-24 15:03:09 +0000

    remove duplicate test

--- tests/api/rooms.spec.ts
+++ tests/api/rooms.spec.ts
@@ -1,33 +1,23 @@
 import { test, expect } from '@playwright/test';
 import { newRoom, updatedRoom } from '../utils/data';
-import { Rooms } from '../entity/rooms.entity';
 import { ApiClient } from '../utils/common';
-import { faker } from '@faker-js/faker';
+
 
 const mockRooms = [newRoom];
 const mockUpdatedRoom = [updatedRoom];
 
-test.describe.serial('Rooms API', () => {
+test.describe('Rooms API', () => {
+
 let apiClient: ApiClient;
 
   test.beforeAll(async () => {
     apiClient = new ApiClient('http://localhost:3000');
-    
   });
   
-  test('GET /posts', async () => {
-    await apiClient.initIfNeeded();
-    const posts = await apiClient.get('/rooms');
-    expect(posts).toBeInstanceOf(Array);
-    expect(posts.length).toBeGreaterThan(0);
-    const firstRoomId = posts[0].id;
-    await apiClient.deleteRoom(firstRoomId);
-  });
-
+ 
   test('Get all rooms - should return a list of rooms', async ({ request }) => {
     const response = await request.get(`/rooms`);
     expect(response.status()).toBe(200);
-
     const rooms = await response.json();
     expect(Array.isArray(rooms)).toBeTruthy();
     expect(rooms.length).toBeGreaterThan(0); // At least one room
@@ -40,7 +30,6 @@ let apiClient: ApiClient;
     expect(response).toHaveProperty('id');
     expect(response).toHaveProperty('price');
     expect(response.id).toBe(roomId.toString());
-         
   });
 
   test('Create new room', async () => {
@@ -49,8 +38,7 @@ let apiClient: ApiClient;
     expect(addResponse).not.toBeUndefined();
     const room  = await addResponse;
     expect(room.id).toEqual(newRoom.id);
-    expect(room.price).toStrictEqual(newRoom.price); 
-
+    expect(room.price).toStrictEqual(newRoom.price);
   });
 
   test('Update existing room', async ({ request }) => {
@@ -63,12 +51,11 @@ let apiClient: ApiClient;
     expect(response.status()).toBe(200);
     const roomType =await response.json().then((data) => data.type)
     expect(roomType).toEqual(updatedRoom.type);
-    console.log(updatedRoom);
   });
 
   test('Delete existing room', async () => {
     await apiClient.initIfNeeded();
-    const roomId: number = await apiClient.getFirstRoomId();  // Gets a list of rooms and returns the first one's id
+    const roomId: number = await apiClient.getFirstRoomId(); 
     const response = await apiClient.deleteRoom(roomId);
     expect(response.status()).toBe(200);
   });
